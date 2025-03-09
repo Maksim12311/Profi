4a5af14b2f6fca86fb3c5abb6c29b5df34429e83
GorodetskiyKP <gorodetskiykp@gmail.com>
2025-03-09 12:06:46 +0300

    Добавил модели

--- backoffice/models.py
+++ backoffice/models.py
@@ -1,3 +1,16 @@
+# https://docs.djangoproject.com/en/5.1/ref/models/fields/
+
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Profile(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
